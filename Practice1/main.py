1adef2b55561f529ebb9acb8a2724fb2c596ca7f
NikitaVenediktov <n.venediktov@gmail.com>
2022-11-16 16:00:54 +0300

    1st practice and 1st try test

---
+++ Practice1/main.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class RecoResponse(BaseModel):
+    user_id: int
+    items: List[int]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/reco/{model_name}/{user_id}", response_model=RecoResponse)
+async def get_reco(model_name: str, user_id: int) -> RecoResponse:
+    if model_name == "kek":
+        reco_list = list(range(20))
+    else:
+        reco_list = list(range(20))[::-1]
+    reco = RecoResponse(
+        user_id=user_id,
+        items=reco_list
+        )
+    return reco
