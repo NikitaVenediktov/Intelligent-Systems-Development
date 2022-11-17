92bc21438091c45385589479662ac344a64c8bc3
Kirill <gipokrat089@gmail.com>
2022-11-17 10:57:59 +0300

    Create dataload.py

---
+++ services/dataload.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+import zipfile as zf
+
+# Download dataset and unzip it
+url = 'https://github.com/irsafilo/KION_DATASET/raw/f69775be31fa5779907cf0a92ddedb70037fb5ae/data_original.zip'
+
+req = requests.get(url, stream=True)
+
+with open('kion.zip', 'wb') as fd:
+    total_size_in_bytes = int(req.headers.get('Content-Length', 0))
+    for chunk in req.iter_content(chunk_size=2 ** 20):
+        fd.write(chunk)
+
+files = zf.ZipFile('kion.zip','r')
+files.extractall()
+files.close()
