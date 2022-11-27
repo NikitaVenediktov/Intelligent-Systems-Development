d3520484907f1aaee34b35561e3533e2117ac60a
Kirill <gipokrat089@gmail.com>
2022-11-27 16:57:42 +0300

    Restored authentication

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pandas as pd
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, Security, status
 from fastapi.security.api_key import APIKey, APIKeyHeader, APIKeyQuery
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi.testclient import TestClient
 from pydantic import BaseModel
 from rectools import Columns
@@ -38,16 +39,20 @@ df_final_reco = pd.read_pickle("./models/user_knn/final_reco.pickle")
 
 api_key_query = APIKeyQuery(name="api_key", auto_error=False)
 api_key_header = APIKeyHeader(name="api_key", auto_error=False)
+token_bearer = HTTPBearer(auto_error=False)
 
 
 async def get_api_key(
     api_key_from_query: str = Security(api_key_query),
     api_key_from_header: str = Security(api_key_header),
+    token: HTTPAuthorizationCredentials = Security(token_bearer)
 ) -> str:
     if api_key_from_query == API_KEY:
         return api_key_from_query
     elif api_key_from_header == API_KEY:
         return api_key_from_header
+    elif token is not None and token.credentials == API_KEY:
+        return token.credentials
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
@@ -90,8 +95,8 @@ async def get_reco(
 
 if __name__ == "__main__":
 
-    host = os.getenv("HOST", "192.168.89.10")  # Вариант для сервера Кирилла
-    # host = os.getenv("HOST", "127.0.0.1")  # Если у себя тестить
+    # host = os.getenv("HOST", "192.168.89.10")  # Вариант для сервера Кирилла
+    host = os.getenv("HOST", "127.0.0.1")  # Если у себя тестить
     port = int(os.getenv("PORT", "8080"))
 
     uvicorn.run(app, host=host, port=port)
